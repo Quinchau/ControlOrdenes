db478c61e2f25c6e40161c61b8cdcf41aaae2ca0
Cesar <tiendaorion2020@gmail.com>
2024-12-11 16:22:58 -0400

    requiriments.txt

--- app12/purchs_page.py
+++ app12/purchs_page.py
@@ -6,7 +6,7 @@ from .components.filter_orders import filter_component
 
 @rx.page(route='/purchs_page', title='purch_page', on_load=[States.on_load, States.get_all_purchs, States.load_locations])
 def index() -> rx.Component:
-    return rx.container(
+    return rx.flex(
         rx.vstack(
             rx.hstack(
                 rx.color_mode.button(),
@@ -17,9 +17,11 @@ def index() -> rx.Component:
             filter_component(),
             table_purchs(States.purchorders),
             direction='column',
-            style={"width": '29vw', 'margin': 'auto'},
             on_mount=States.get_all_purchs,
-        )
+        ),
+        # justify="center",
+
+
     )
 
 
