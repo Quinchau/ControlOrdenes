4bbf4443ca324289ff656d4e46412a79be2db543
Cesar <tiendaorion2020@gmail.com>
2024-12-12 00:22:13 -0400

    Purchs_page

--- app12/purchs_page.py
+++ app12/purchs_page.py
@@ -7,13 +7,16 @@ from .components.filter_orders import filter_component
 @rx.page(route='/purchs_page', title='purch_page', on_load=[States.on_load, States.get_all_purchs, States.load_locations])
 def index() -> rx.Component:
     return rx.flex(
-        rx.hstack(
-            rx.color_mode.button(),
-            rx.heading('Ordenes Mary Kay', align='center'),
+        rx.vstack(
+            rx.hstack(rx.heading('Ordenes Mary Kay', align='center')),
+            rx.hstack(
+                filter_component(), rx.color_mode.button(),
+                align="center",
+                justify="between",
+                width="100%",
+            ),
+            table_purchs(States.purchorders)
         ),
-
-        filter_component(),
-        table_purchs(States.purchorders),
         direction='column',
         align='center',
         on_mount=States.get_all_purchs,
