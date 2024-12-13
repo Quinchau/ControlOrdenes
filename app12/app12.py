424b28e6468f8f6b6c23fa1755f408d131ce4813
Cesar <tiendaorion2020@gmail.com>
2024-12-12 20:15:53 -0400

    pantalla inicio

--- app12/app12.py
+++ app12/app12.py
@@ -13,19 +13,29 @@ class State(rx.State):
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
+    return rx.flex(
         rx.vstack(
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+            rx.button("Gestion Mary Kay", width='40vh',
+                      height='8vh', radius='full',  on_click=rx.redirect("/purchs_page")),
+            rx.button("Gestion Amazon", width='40vh',
+                      height='8vh',
+                      radius='full', disable='True', on_click=rx.toast("Próximamente...")),
+            rx.button("Otros", width='40vh',
+                      height='8vh', radius='full', disable='True', on_click=rx.toast("Próximamente...")),
+            rx.code("to-do-easy.com", size="6", weight="bold"),
+            border_width="2px",
+            border_radius="1em",
+            align='center',
+            width='100vh',
+            height="100vh",
+            justify='center',
+            max_width='400px',
+            spacing="9",
+            background_image="url('/drops-6392473_640.jpg')",
+
+
         ),
-        rx.logo(),
+        justify='center'
     )
 
 
